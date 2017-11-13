eccc719f5e75ecf16fdd97bb932927aa5632a2f0
justin lee <yuminjustin@163.com>
2017-11-13 14:06:52 +0800

    Update meta.js

--- meta.js
+++ meta.js
@@ -15,5 +15,6 @@ module.exports = {
       "type": "string",
       "message": "Author"
     },
-  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+    "completeMessage": "开始你的项目:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\n"
+  }
 };
