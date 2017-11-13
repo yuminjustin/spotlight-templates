d96670670f685f7bb53bc25be1721862b35918d8
justin lee <yuminjustin@163.com>
2017-11-13 14:05:32 +0800

    Create meta.js

---
+++ meta.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "message": "Project name"
+    },
+    "description": {
+      "type": "string",
+      "required": false,
+      "message": "Project description",
+      "default": "A babckbone project"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    },
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+};
