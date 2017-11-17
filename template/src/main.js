b8eb56788daa305ca9945e32f03569a19eb6757b
justin lee <yuminjustin@163.com>
2017-11-17 17:29:40 +0800

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -4,5 +4,11 @@ require('@/assets/css/reset.css');
 require('@/assets/css/page.css');
 var Router = require('./common/router')
 var container = $("#app");
+
 new Router(container);
 B.history.start();
+
+/* webpack 热替换 */
+if (module.hot) {
+    module.hot.accept();
+}
