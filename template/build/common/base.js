a374e7c6297afcbc1f936a1b0274e3843da787be
lixiaolong <lixiaolong1063@stnts.com>
2018-09-12 17:39:43 +0800

    Upgrade to webpack4

---
+++ template/build/common/base.js
@@ -0,0 +1,57 @@
+/* webpack基础配置 */
+var webpack = require('webpack')
+var path = require('path')
+var config = require("../config")
+
+
+module.exports = function (isProd) {
+    var codeBase = path.join(__dirname, '../../src'),
+        _dir = isProd ? './static' : 'static';
+    return {
+        entry: config.entry,
+        resolve: {
+            extensions: ['.js', '.json'],
+            alias: {
+                '@': codeBase,
+                'C': path.join(__dirname, '../../src/components')
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: path.posix.join(_dir, 'image/[name].[hash:5].[ext]') //image 文件夹
+                    }
+                },
+                {
+                    test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: path.posix.join(_dir, 'media/[name].[hash:5].[ext]') //media 文件夹
+                    }
+                },
+                {
+                    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: path.posix.join(_dir, 'fonts/[name].[hash:5].[ext]') //fonts 文件夹
+                    }
+                }
+            ]
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                "$": "jquery",
+                "jQuery": "jquery",
+                "B": "backbone",
+                "_": "underscore",
+                "_S": "underscore.string"
+            })
+        ]
+    }
+}
--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,18 +1,26 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 var uglyfly = require('gulp-uglyfly');
+var cleanCSS = require('gulp-clean-css');
 var config = require('./build/config');
 
-var root = config.build.gulpPath;
+var root = config.build.outputPathName;
 
 gulp.task('replace', function () {
     gulp.src(root + '/static/js/*.js')
         .pipe(replace('.catch(', "['catch'](")) // 解决IE不兼容
+        .pipe(replace('iterator.return', "iterator['return']")) 
         .pipe(uglyfly())
         .pipe(gulp.dest(root + '/static/js/'));
+
+    gulp.src(root + '/static/css/*.css')
+        .pipe(cleanCSS({
+            compatibility: 'ie6'
+        }))
+        .pipe(gulp.dest(root + '/static/css/'));
 })
 
 
-gulp.task('default', ['replace'],function(){
+gulp.task('default', ['replace'], function () {
     console.log('打包成功.')
 })
