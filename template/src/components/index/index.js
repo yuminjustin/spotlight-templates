d6d69379608bca80151483f986a5c9f46ff279ae
lixiaolong <lixiaolong1063@stnts.com>
2018-09-13 16:19:38 +0800

    图片替换

--- template/src/components/index/index.js
+++ template/src/components/index/index.js
@@ -1,6 +1,6 @@
 var tpl = require('html-loader!./tpl/index.html');
 var Base = require('@/common/base');
-var img = require('@/assets/image/sp_black.png')
+var img = require('@/assets/image/black.png')
 
 var View = Base.View.extend({
     source:{
